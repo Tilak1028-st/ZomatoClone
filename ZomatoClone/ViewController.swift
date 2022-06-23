ab9403e8fdbff3f3e6277274c1034b7be0cb6873
Shakya Tilak <75114840+Tilak1028-st@users.noreply.github.com>
2022-06-23 00:16:55 -0700

    Initial Commit

---
+++ ZomatoClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZomatoClone
+//
+//  Created by PCS213 on 23/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
